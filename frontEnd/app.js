19ee5cb1e8832a07f497f269e170650296503b0f
FemilaCS <167897198+FemilaCS@users.noreply.github.com>
2026-03-08 22:54:22 +0530

    basic frontend layout

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -1,11 +1,14 @@
 const loginBtn = document.querySelector(".login-button");
 const popup = document.querySelector(".login-popup");
 const closeBtn = document.querySelector(".back");
+const blur_effect = document.querySelector(".blur-effect");
 
 loginBtn.onclick = function(){
     popup.style.display = "block";
+    blur_effect.style.display = "block"
 }
 
 closeBtn.onclick = function(){
     popup.style.display = "none";
+    blur_effect.style.display = "none";
 }
